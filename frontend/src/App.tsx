c4f2b4fded83a2a09b7aff2eabcc7f109231c0a5
alexreynlds <ajrey@live.co.uk>
2025-05-06 17:25:15 +0100

    + Added shadcn/ui as a component library

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div className="bg-red-500 p-2">
       <h1>Hello, World!</h1>
+      <Button>Press me!</Button>
     </div>
   );
 }
