b201b3f282f436487c87b2df349c51fa1c8ce356
alexreynlds <ajrey@live.co.uk>
2025-05-06 17:38:17 +0100

    + Added the aesthetics for the login page, using toast to confirm login - non functional just sends user to dashboard

--- frontend/src/LoginPage.tsx
+++ frontend/src/LoginPage.tsx
@@ -1,18 +1,73 @@
-import { useNavigate } from 'react-router';
+import './index.css';
 import { Button } from './components/ui/button';
+import { Input } from './components/ui/input';
+import { Label } from './components/ui/label';
+import { useState } from 'react';
+import useAuth from './hook/Auth';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
 
 function LoginPage() {
   const navigate = useNavigate();
+
+  // Variables to store the login info typed into the inputs
+  const [loginInfo, setLoginInfo] = useState({
+    email: '',
+    password: '',
+  });
+
+  // Handle the change in the input fields for login and register
+  // setting the login info to the values of the input fields
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLoginInfo((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    toast.success('Login successful!');
+    navigate('/dashboard');
+  };
+
   return (
-    <div className="bg-red-500 p-2">
-      <h1>Hello, World!</h1>
-      <Button
-        onClick={() => {
-          navigate('/dashboard');
-        }}
+    <div className="flex h-screen w-full items-center justify-center bg-[#272727] text-white">
+      <form
+        onSubmit={handleLogin}
+        className="flex h-[60%] min-w-[500px] flex-col items-center justify-between rounded-lg border-2 border-[#272727] bg-[#1e1e1e] p-8 shadow-lg"
       >
-        Press me!
-      </Button>
+        <h1 className="text-2xl font-bold underline">LOG IN</h1>
+        <div className="flex w-full flex-col gap-4">
+          <Label htmlFor="email" className="text-sm font-medium">
+            Email
+          </Label>
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            value={loginInfo.email}
+            onChange={handleChange}
+          />
+          <Label htmlFor="password" className="text-sm font-medium">
+            Password
+          </Label>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            value={loginInfo.password}
+            onChange={handleChange}
+          />
+        </div>
+        <Button
+          type="submit"
+          className="cursor-pointer bg-[#272727] text-white hover:bg-[#1c1c1c]"
+        >
+          <span className="text-sm font-medium">Login</span>
+        </Button>
+      </form>
     </div>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,5 @@
 import ReactDom from 'react-dom/client';
+import { Toaster } from 'sonner';
 import { StrictMode } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import './index.css';
@@ -12,6 +13,7 @@ ReactDom.createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
+      <Toaster/>
     </BrowserRouter>
   </StrictMode>,
 );
